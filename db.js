7fbf3f7da7d528c645c81c6da7a7559ad6e96de5
James Schwartz <jschwartzdev@gmail.com>
2024-04-17 09:50:56 -0400

    made ready for deployment

--- db.js
+++ db.js
@@ -1,12 +1,28 @@
-require("dotenv").config();
-const { Pool } = require("pg");
+// require("dotenv").config();
+// const { Pool } = require("pg");
 
+// const pool = new Pool({
+//   host: "localhost",
+//   user: "postgres",
+//   port: 5432,
+//   password: process.env.DB_PASSWORD,
+//   database: "cardharbor",
+// });
+
+// module.exports = pool;
+
+const { Pool } = require("pg");
+require("dotenv").config();
+let { PGHOST, PGDATABASE, PGUSER, PGPASSWORD } = process.env;
 const pool = new Pool({
-  host: "localhost",
-  user: "postgres",
+  host: PGHOST,
+  database: PGDATABASE,
+  username: PGUSER,
+  password: PGPASSWORD,
   port: 5432,
-  password: process.env.DB_PASSWORD,
-  database: "cardharbor",
+  ssl: {
+    require: true,
+  },
 });
 
 module.exports = pool;
